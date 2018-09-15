dbf8416515d477356adc69c7edb5aabdff427ffb
Darrin Howell <dbhowell09@gmail.com>
2018-09-15 14:29:43 -0700

    Cleaned up code in q2 to ensure that output runs smoothly

--- app.js
+++ app.js
@@ -32,9 +32,9 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-  return [];
-}
+// function multiply(a, b) { //eslint-disable-line
+
+// }
 
 // Here is the test for multiply(); uncomment it to run it
 //testMultiply();
